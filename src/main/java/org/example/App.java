72a1d546e0ed47b8ea742dced2a2e57ddcab35f7
LENOVO <arlen.kubamychbekovv01@gamil.com>
2022-07-28 18:58:13 +0300

    branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!");
         System.out.println("Nurtilek");
         System.out.println("Men jany branch koshtum e");
+        System.out.println("branch dagy koshtum");
     }
 }
